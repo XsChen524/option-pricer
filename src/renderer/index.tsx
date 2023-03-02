1dc8a70134ff75b70205d14080a7ad33dc6be1e6
XsChen524 <cxs52hku@connect.hku.hk>
2023-03-03 06:06:07 +0800

    index.tsx

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import AppRouter from "router";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
-root.render(<App />);
+root.render(<AppRouter />);
 
 // calling IPC exposed from preload script
 window.electron.ipcRenderer.once("ipc-example", (arg) => {

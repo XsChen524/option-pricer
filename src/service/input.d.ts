aa06b035401921cd6e77eafcc7c997d07dd424e5
XsChen524 <cxs52hku@connect.hku.hk>
2023-03-03 22:18:33 +0800

    Update ebs params

--- src/service/input.d.ts
+++ src/service/input.d.ts
@@ -1,7 +1,16 @@
+export interface ExtendedBSRawParams {
+	spot: string;
+	strike: string;
+	termToMaturity: string;
+	riskFreeRate: string;
+	repoRate: string;
+	volatility: string;
+	optionType: string;
+}
 export interface ExtendedBSParams {
 	spot: number;
 	strike: number;
-	timeToMaturity: number;
+	termToMaturity: number;
 	riskFreeRate: number;
 	repoRate: number;
 	volatility: number;

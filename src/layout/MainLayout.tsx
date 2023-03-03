2f278c810e2edf231ac40739c7d32fda41acb9cb
XsChen524 <cxs52hku@connect.hku.hk>
2023-03-03 09:04:48 +0800

    Stylesheets

---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { FunctionOutlined } from "@ant-design/icons";
+import { Layout, Menu } from "antd";
+import type { MenuProps } from "antd";
+import { useNavigate } from "react-router-dom";
+import AppRouter from "router";
+import "../style/layout/mainLayoutStyle.css";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const MainLayout: React.FC<{}> = () => {
+	const [title, setTitle] = useState<string>("Option Pricer");
+	const navigate = useNavigate();
+
+	const items: MenuProps["items"] = [
+		{
+			key: "1",
+			icon: React.createElement(FunctionOutlined),
+			label: "Extended BS",
+			onClick: () => {
+				setTitle("Entended Black Scholes");
+				navigate("ebs");
+			},
+		},
+		{
+			key: "2",
+			icon: React.createElement(FunctionOutlined),
+			label: "Implied Volatility",
+			onClick: () => {
+				setTitle("Implied Volatility");
+				navigate("iv");
+			},
+		},
+	];
+
+	return (
+		<Layout hasSider>
+			<Sider id="main-layout-sider" breakpoint="sm" collapsedWidth="0">
+				<Menu theme="dark" mode="inline" items={items} />
+			</Sider>
+			<Layout className="site-layout" style={{ marginLeft: 200 }}>
+				<Header id="main-layout-header">
+					<h3>{title}</h3>
+				</Header>
+				<Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+					<div id="main-layout-content-container">
+						<AppRouter />
+					</div>
+				</Content>
+				<Footer style={{ textAlign: "center" }}>
+					© 2023 Chen Xingsheng
+				</Footer>
+			</Layout>
+		</Layout>
+	);
+};
+
+export default MainLayout;

72c79219764095182ea11e209a12ec0358bb4c60
XsChen524 <cxs52hku@connect.hku.hk>
2023-03-03 09:06:03 +0800

    Update views

---
+++ src/view/ExtendedBS.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ExtendedBSView: React.FunctionComponent<{}> = () => {
+	return (
+		<div>
+			<p>Extended BS</p>
+		</div>
+	);
+};
+
+export default ExtendedBSView;
---
+++ src/view/IvView.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const IvView: React.FunctionComponent<{}> = () => {
+	return (
+		<div>
+			<p>Implied Volatility</p>
+		</div>
+	);
+};
+
+export default IvView;
